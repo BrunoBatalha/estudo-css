2da46b0f7280f049f9dd228269a2ca10b309e239
Bruno Batalha <brunobatalha37@gmail.com>
2020-08-14 15:15:10 -0400

    Cria animação ao abri menu

--- menu_responsivo/script.js
+++ menu_responsivo/script.js
@@ -1,17 +1,6 @@
-window.onload = () => init();
 function toggleMenu() {
-    const menus = document.querySelector('.menus');
-    menus.style.display =
-        menus.style.display === 'block' ? 'none' : 'block'
-}
-function init() {
-
-    window.onresize = function (e) {
-        const menus = document.querySelector('.menus');
-        if (e.target.innerWidth > 600) {
-            menus.style.display = 'block';
-        } else {
-            menus.style.display = 'none';
-        }
-    }
+  const menu = document.querySelector(".menus");
+  const ul = document.querySelector(".menus > ul");
+  menu.classList.toggle("open");
+  ul.classList.toggle("d-block");
 }
