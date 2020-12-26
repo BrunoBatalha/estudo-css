da4232060ba92ddacc5917a8457f721fe79fdbaf
BrunoBatalha <brunobatalha37@gmail.com>
2020-12-26 14:16:51 -0400

    Deixa sempre somente 50 circulos seguindo

--- circulo_segue_mouse/script.js
+++ circulo_segue_mouse/script.js
@@ -1,20 +1,26 @@
-
 window.onload = () => init();
 function init() {
-    const quadrado = document.querySelector('.quadrado');
+  const quadrado = document.querySelector("#quadrado-main");
 
-    let contador = 0;
+  document.onmousemove = function (e) {
+    quadrado.style.left = e.pageX - quadrado.offsetHeight / 2 + "px";
+    quadrado.style.top = e.pageY - quadrado.offsetWidth / 2 + "px";
 
-    document.onmousemove = function (e) {
+    const clone = quadrado.cloneNode(true);
+    clone.style.zIndex = 1;
+    clone.id = "";
 
-        quadrado.style.left = (e.pageX - quadrado.offsetHeight / 2) + 'px';
-        quadrado.style.top = (e.pageY - quadrado.offsetWidth / 2) + 'px';
-
-        const clone = quadrado.cloneNode(true);
-        clone.style.zIndex = 1;
-
-        document.body.appendChild(clone);
-
+    document.body.appendChild(clone);
+  };
+  function run() {
+    const quadrados = document.querySelectorAll(".quadrado");
+    if (quadrados.length > 5) {
+      for (let i = 1; i < quadrados.length - 50; i++) {
+        quadrados.item(i).remove();
+      }
     }
 
+    window.requestAnimationFrame(run);
+  }
+  run();
 }
